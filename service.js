934b2929a81557501a481d0f4c5418280266015c
marcandy <marcandy.nj@gmail.com>
2016-12-15 12:14:05 -0700

    review

--- service.js
+++ service.js
@@ -7,10 +7,16 @@ angular.module('userProfiles').service('mainService', function($http, $q) {
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
     }).then(function (response) {
-      deferred.resolve(response.data.data);
+      var parsedResponse = response.data.data;
+      for (var i = 0; i < parsedResponse.length; i++) {
+        parsedResponse[i].first_name = 'Ralf';
+      }
+      deferred.resolve(parsedResponse);
     })
+
+    return deferred.promise;
   }
 
-  return deferred.promise;
+
 
 });
